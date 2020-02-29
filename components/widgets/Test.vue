d972da3d33639ded8fa5fbbd1cc1de96a6528146
Piscinelove <rafael.peixoto@iomedia.ch>
2020-02-29 13:04:29 +0000

    Update

---
+++ components/widgets/Test.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>Widget Title</h1>
+  <p>{{ test }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'Title'
+    }
+  }
+}
+</script
---
+++ static/test/packages.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueCustomElement from 'vue-custom-element';
+
+import Component0 from '@/components/widgets/Test.vue';
+
+Vue.use(VueCustomElement);
+
+async function polyfill () {
+  if (!(('customElements' in window) || ('registerElement' in document))) {
+    return await import('document-register-element/build/document-register-element'); // For cross-browser compatibility (IE9+) use Custom Elements polyfill.
+  } else {
+    return;
+  }
+}
+
+polyfill().then(() => {
+  Vue.customElement('netplus-test-packages', Component0, {"shadow":false,"props":{"basePath":"/"}});
+
+  // Create missing NuxtLink as RouterLink.
+  Vue.component('NuxtLink', {
+    extends: Vue.component('RouterLink')
+  })
+});
