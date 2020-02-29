afbb931670b73f397c9e647efaf0115416010c96
Piscinelove <rafael.peixoto@iomedia.ch>
2020-02-29 16:17:32 +0000

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,6 @@ export default {
                     test:'test',
                     pktuveuxpas:'pktuveuxpas'
                   },
-                  shadow: true
                 },
               },
           ]
